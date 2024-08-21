505faaa7a06c64ecae4fc3201330ffe87256e22f
Irs444 <irs786had123@gmail.com>
2024-08-21 13:15:55 +0530

    detail page with footer

--- client/screens/Home.jsx
+++ client/screens/Home.jsx
@@ -4,10 +4,12 @@ import Layout from '../components/Layout/Layout'
 import Categories from '../components/category/Categories'
 import Banner from '../components/banner/Banner'
 import Product from '../components/products/Product'
+import Header from '../components/Layout/Header'
 
 const Home = () => {
     return (
         <Layout>
+            <Header />
             <Categories/>
             <Banner/>
             <Product/>
--- client/screens/ProductDetail.jsx
+++ client/screens/ProductDetail.jsx
@@ -1,26 +1,112 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
 import React, { useEffect, useState } from 'react'
 import { productData } from '../data/productData'
+import Layout from '../components/Layout/Layout'
 
-const ProductDetail = ({route}) => {
+const ProductDetail = ({ route }) => {
 
-  const {params} = route
-  const [pDetails , setPDetails] = useState({})
+  const { params } = route
+  const [pDetails, setPDetails] = useState({})
+  const [qty, setQty] = useState(1)
+
   // get product detail
-   useEffect(() => {
-      const getData = productData.find((p) => {
-        return p?._id === params?._id
-      })
+  useEffect(() => {
+    const getData = productData.find((p) => {
+      return p?._id === params?._id
+    })
 
-      setPDetails(getData)
-   }, [params?._id])
+    setPDetails(getData)
+  }, [params?._id])
+
+  const handleIncrement = () => {
+    if (qty === 10) return alert("You can't add more than 10 quantity")
+    setQty(qty + 1)
+  }
+
+  const handleDecerement = () => {
+    if (qty <= 1) return
+    setQty(qty - 1)
+  }
   return (
-    <View>
-      <Text>{JSON.stringify(pDetails)}</Text>
-    </View>
+    <Layout >
+      <Image source={{ uri: pDetails?.imageUrl }} style={styles.image} />
+
+      <View style={styles.container} >
+
+        <Text style={styles.name}>{pDetails?.name}</Text>
+        <Text style={styles.name}>Price: ₹ {pDetails?.price}</Text>
+        <Text><Text style={{ fontWeight: "bold" }}>Details:</Text> {pDetails?.description}</Text>
+
+        <View style={styles.btnContainer}>
+
+          <TouchableOpacity style={styles.cart}  onPress={() => alert(`${qty} items are added to cart` )}>
+            <Text style={styles.cartText}>ADD TO CART</Text>
+          </TouchableOpacity>
+
+          <View style={styles.btnContainer}>
+            <TouchableOpacity style={styles.btnQty} onPress={() => handleDecerement()}>
+              <Text style={styles.btnText}>-</Text>
+            </TouchableOpacity>
+            <Text style={{ fontSize: 20 }}>{qty}</Text>
+            <TouchableOpacity style={styles.btnQty} onPress={() => handleIncrement()}>
+              <Text style={styles.btnText}>+</Text>
+            </TouchableOpacity>
+          </View>
+
+        </View>
+      </View>
+    </Layout>
   )
 }
 
 export default ProductDetail
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  image: {
+    height: 400,
+    width: "100%"
+  },
+  container: {
+    marginTop: 10,
+    paddingHorizontal: 10
+  },
+  name: {
+    fontSize: 15,
+    fontWeight: "bold",
+    marginBottom: 5
+  },
+  btnContainer: {
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    marginHorizontal: 10,
+    marginVertical: 20
+  },
+  cart: {
+    backgroundColor: "#597445",
+    borderRadius: 5,
+    height: 40,
+    width: 150,
+    justifyContent: "center",
+  },
+  cartText: {
+    textAlign: "center",
+    color: "white",
+    fontWeight: "bold"
+  },
+  btnQty: {
+    backgroundColor: "#729762",
+    borderRadius: 5,
+    height: 40,
+    width: 40,
+    justifyContent: "center",
+    alignItems: "center",
+    marginHorizontal: 5
+  },
+  btnText: {
+    fontSize: 20,
+    color: "white",
+    fontWeight: "bold"
+  }
+
+})
