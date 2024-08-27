49924c536940605d7c5cf9e0c521c79012b2daeb
Irs444 <irs786had123@gmail.com>
2024-08-27 21:38:32 +0530

    ...

--- server/routes/testRoute.js
+++ server/routes/testRoute.js
@@ -5,9 +5,7 @@ const testControllers = require("../controllers/testControllers.js")
 
 
 router.get("/" , testControllers)
-router.get("/" , testControllers)
-router.get("/" , testControllers)
-router.get("/" , testControllers)
+
 
 
 module.exports =  router
